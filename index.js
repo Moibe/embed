10b1bc2a3aef9be343585898c35298a5be51f32b
Moibe <moi_estrello@hotmail.com>
2024-02-27 14:12:21 -0600

    HF embed standby

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const localStorage = window.localStorage;
 
 // Recuperar una variable de localStorage
-const nombre = localStorage.getItem('nombre');
+var nombre = localStorage.getItem('nombre');
 console.log("El nombre guardado es:", nombre)
 
 // Guardar una variable en localStorage
@@ -11,10 +11,7 @@ localStorage.setItem('nombre', 'Pedro');
 
 // Recuperar una variable de localStorage
 nombre = localStorage.getItem('nombre');
-console.log("El nombre guardado es:", nombre)
-
 console.log("Ahora el nuevo nombre guardado es:", nombre)
-
 
 // Eliminar una variable de localStorage
 //localStorage.removeItem('nombre');
