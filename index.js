d3c78d5153261ac1c3941d61f34e83d2acdf9694
Moibe <moi_estrello@hotmail.com>
2024-02-27 13:29:56 -0600

    Primer commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Importar el objeto localStorage
+const localStorage = window.localStorage;
+
+// Guardar una variable en localStorage
+localStorage.setItem('nombre', 'Juan');
+
+// Recuperar una variable de localStorage
+const nombre = localStorage.getItem('nombre');
+
+// Eliminar una variable de localStorage
+//localStorage.removeItem('nombre');
