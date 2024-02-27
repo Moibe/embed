cc95cccfbe7eabbf90fc9a0872d60de5e931ccb1
Moibe <moi_estrello@hotmail.com>
2024-02-27 14:05:39 -0600

    Rename

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 // Importar el objeto localStorage
 const localStorage = window.localStorage;
 
-// Guardar una variable en localStorage
-localStorage.setItem('nombre', 'Juan');
-
 // Recuperar una variable de localStorage
 const nombre = localStorage.getItem('nombre');
 console.log("El nombre guardado es:", nombre)
+
+// Guardar una variable en localStorage
+localStorage.setItem('nombre', 'Pedro');
+
+
+// Recuperar una variable de localStorage
+nombre = localStorage.getItem('nombre');
+console.log("El nombre guardado es:", nombre)
+
+console.log("Ahora el nuevo nombre guardado es:", nombre)
+
 
 // Eliminar una variable de localStorage
 //localStorage.removeItem('nombre');
